4ce57efdad4be3d95e57cda8f5ff7375c00262b1
eduardomaldonadofernandez21 <edumaldonadofdez21@gmail.com>
2024-03-12 18:15:32 +0000

    Image-slider

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
 
 function App() {
   return (
@@ -14,7 +15,14 @@ function App() {
       {/* <RandomColor/> */}
 
       {/* Star rating Component*/}
-      <StarRating/>
+      {/*<StarRating/> */}
+
+      {/* Image Slider Component*/}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"3"}
+        limit={"5"}
+      />
     </div>
   );
 }
