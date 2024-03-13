e6016a80750ad5f7fa9482093651492ca0d65702
eduardomaldonadofernandez21 <edumaldonadofdez21@gmail.com>
2024-03-13 15:39:22 +0000

    scroll-indicator

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Accordian from './components/accordian';
-import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
-import ImageSlider from './components/image-slider';
-import LoadMoreData from './components/load-more-data';
+import logo from "./logo.svg";
+import "./App.css";
+import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
 import TreeView from "./components/tree-view";
 import menus from "./components/tree-view/data";
-import QRCodeGenerator from './components/qr-code-generator';
-import LightDarkMode from './components/light-dark-mode';
+import QRCodeGenerator from "./components/qr-code-generator";
+import LightDarkMode from "./components/light-dark-mode";
+import ScrollIndicator from "./components/scroll-indicator";
 
 function App() {
   return (
@@ -38,7 +39,10 @@ function App() {
       {/*<QRCodeGenerator/>*/}
 
       {/* light and dark theme switch */}
-      <LightDarkMode />
+      {/* <LightDarkMode /> */}
+
+      {/* Scroll Indicator Component */}
+      <ScrollIndicator  url={"https://dummyjson.com/products?limit=100"} /> 
     </div>
   );
 }
