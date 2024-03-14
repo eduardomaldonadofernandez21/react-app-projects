5bb4803d8e702cca5a8897c3a0aff5895bc27b1d
eduardomaldonadofernandez21 <edumaldonadofdez21@gmail.com>
2024-03-14 12:09:56 +0000

    useWindowResize

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import FeatureFlagGlobalState from "./components/feature-flag/context";
 import FeatureFlags from "./components/feature-flag";
 import UseFetchHookTest from "./components/use-fetch/test";
 import UseOnclickOutsideTest from "./components/use-outside-click/test";
+import UseWindowResizeTest from "./components/use-window-resize/test";
 
 function App() {
   return (
@@ -78,6 +79,10 @@ function App() {
 
       {/* Use Onclick Outside Hook Test */}
       {/* <UseOnclickOutsideTest/> */}
+
+      {/* Use Window Resize Hook Test */}
+      <UseWindowResizeTest/>
+
     </div>
   );
 }
