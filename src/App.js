ea4325d4bbc2b52e1947dc977ec59aa9f21fd95a
eduardomaldonadofernandez21 <edumaldonadofdez21@gmail.com>
2024-03-14 11:05:53 +0000

    tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import TabTest from "./components/custom-tabs/tab-test";
 import ModalTest from "./components/custom-modal-popup/modal-test";
 import GithubProfileFinder from "./components/github-profile-finder";
 import SearchAutocomplete from "./components/search-autocomplete-with-api";
+import TicTacToe from "./components/tic-tac-toe";
 
 function App() {
   return (
@@ -58,7 +59,10 @@ function App() {
       {/* <GithubProfileFinder/> */}
 
       {/* Search autocomplete with API Component */}
-      <SearchAutocomplete/>
+      {/* <SearchAutocomplete/> */}
+
+      {/* Tic tac toe game Component */}
+      <TicTacToe/>
     </div>
   );
 }
